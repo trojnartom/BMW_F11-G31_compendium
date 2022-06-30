86b7086746adc790e0bd38630d24e339e7c11164
trojnartom <trojnar.t@hotmail.com>
2022-06-30 16:44:43 +0200

    Initial commit

---
+++ src/main/java/com/example/bmw_f11_g31_compendium/BmwF11G31CompendiumApplication.java
@@ -0,0 +1,13 @@
+package com.example.bmw_f11_g31_compendium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmwF11G31CompendiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmwF11G31CompendiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bmw_f11_g31_compendium/BmwF11G31CompendiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bmw_f11_g31_compendium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmwF11G31CompendiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
